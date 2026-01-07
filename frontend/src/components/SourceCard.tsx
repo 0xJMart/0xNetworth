7367abdd83febc46108353ca60d3ec9dced4196f
0xJmart <joemartinez@Joes-MacBook-Pro.local>
2026-01-07 17:33:47 -0500

    fix: Remove unused import in SourceCard

--- frontend/src/components/SourceCard.tsx
+++ frontend/src/components/SourceCard.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { YouTubeSource, updateSourceSchedule } from '../api';
+import { YouTubeSource } from '../api';
 import { formatDate } from '../utils/date';
 
 interface SourceCardProps {
